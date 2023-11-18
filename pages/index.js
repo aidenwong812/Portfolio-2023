a9a7b21060bf1e10693795f2184abc43a4819b37
lionstar <lionstar259007@gmail.com>
2023-11-18 12:12:31 +0900

    change setting

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Ubuntu from "../components/ubuntu";
 import ReactGA from 'react-ga4';
 import Meta from "../components/SEO/Meta";
 
-const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+// const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID;
+const TRACKING_ID = "My Tracking ID";
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
